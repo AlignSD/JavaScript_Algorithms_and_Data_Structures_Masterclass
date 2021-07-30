ab4bb7346e7a941525df9ba9601efa7b1f6c131c
Evan Alioto <EvanJAlioto@gmail.com>
2021-07-30 00:34:54 -0700

    frequency counter added to 5_problem_solving_patterns

--- 5_problem-solving-patterns/frequency-counter-pattern.js
+++ 5_problem-solving-patterns/frequency-counter-pattern.js
@@ -1,15 +1,15 @@
-/* this pattern uses objects or sets to collect values/frequencies of values
+/* 
+this pattern uses objects or sets to collect values/frequencies of values
+this can often avoid the need for nested loops or O(n^2) operations with arrays/strings 
 
-this can often avoid the need for nested loops or O(n^2) operations with arrays/strings */
-
-/* Write a function called same, which accepts two arrays. The function should return true if every value in the 
+Write a function called same, which accepts two arrays. The function should return true if every value in the 
 array has it's corresponding values squared in the second array. The frequency of values must be the same 
 
 ex. same([1,2,3], [4,1,9]) // true
     same([1,2,1], [4,4,1]) // false (must be same frequency)
 */
 
-// naive approach
+// naive approach O(n^2) quadratic time.
 function same(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -22,8 +22,37 @@ function same(arr1, arr2) {
     }
     arr2.splice(correctIndex, 1);
   }
-  console.log("true");
+  console.log("same() true");
   return true;
 }
 
-same([1,2,3],[9,1,4]);
+// same([1,2,3],[9,1,4]);
+
+
+// Frequency counter approach O(n) Linear time.
+function freq(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+  console.log(frequencyCounter1, "freq counter 1");
+  console.log(frequencyCounter2, "freq counter 2");
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false
+    }
+  }
+  return true;
+}
+
+freq([1,2,3, 11, 2],[9,1,4,5, 4]);
